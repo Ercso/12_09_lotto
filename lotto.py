4662ee4cc2b996a4067d8c6c9559fecc62b0e8de
Szalkay Erik <szalkay.erik@diak.szbi-pg.hu>
2024-12-09 08:56:23 +0100

    Create lotto.py

---
+++ lotto.py
@@ -0,0 +1,12 @@
+import random
+
+def lotto1():
+    print(f"Szép napot uram! Szeretnéd tesztelni a szerencséd a mai napon egy kis lottóval?")
+    szamok = int(input("Kérlek adj meg 5 db számot!"))
+    random.randint(1,90)
+    print(random)
+
+    for i in range (0,5)
+        
+
+lotto1()
