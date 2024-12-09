eef2cd8f836434f1b4e9c864d0697c3ae5fcc100
Szalkay Erik <szalkay.erik@diak.szbi-pg.hu>
2024-12-09 09:26:10 +0100

    Update lotto.py

--- lotto.py
+++ lotto.py
@@ -1,13 +1,35 @@
 import random
 
-def lotto1():
-    print(f"Szép napot uram! Szeretnéd tesztelni a szerencséd a mai napon egy kis lottóval?")
-    szamok = int(input("Kérlek adj meg 5 db számot!"))
-    random.randint(1,90)
-    print(random)
+def lottó_játék():
+    print("Üdvözöljük a lottó játékban!")
+    print("Válasszon egy játékot:")
+    print("1. 5/90 lottó")
+    print("2. 6/49 lottó")
+    
+    választás = input("Kérem, válasszon (1 vagy 2): ")
+    
+    if választás == '1':
+        számok = 5
+        max_szám = 90
+    elif választás == '2':
+        számok = 6
+        max_szám = 49
+    else:
+        print("Érvénytelen választás!")
+        return
 
-    for i in range (0,5):
-        int(input(szamok))
-        
+    tipp = int(input("Kérlek adj meg egy számot! "))
+    print(f"Általad generált számok: {tipp}")
+    
+    nyerő_számok =
+    print(f"A nyerő számok:)
+    
+    találatok = 
+    print(f"Találatok száma: {len(találatok)}")
+    if találatok:
+        print(f"Az általad eltalált számok: {sorted(találatok)}")
+    else:
+        print("Sajnos nem talált el egy számot sem.")
 
-lotto1()
+if __name__ == "__main__":
+    lottó_játék()
